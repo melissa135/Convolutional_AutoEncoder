eeec82088630bd8bccdc234b41f00891d51e3276
melissa135 <1071560244@qq.com>
2017-09-16 22:57:29 +0800

    Add files via upload

---
+++ define_network.py
@@ -0,0 +1,52 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+
+class AutoEncoder(nn.Module):
+    def __init__(self):
+        super(AutoEncoder, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv1d(1,5,4,stride=4),
+            nn.Tanh(),
+            nn.Conv1d(5,10,4,stride=4,padding=1),
+            nn.Tanh(),
+            nn.Conv1d(10,5,3,stride=3),
+            nn.Tanh()
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.ConvTranspose1d(5,10,3,stride=3),
+            nn.Tanh(),
+            nn.ConvTranspose1d(10,5,4,stride=4,padding=1),
+            nn.Tanh(),
+            nn.ConvTranspose1d(5,1,4,stride=4)
+        )
+
+    def forward(self, x):
+        
+        x = self.encoder(x)
+	x = self.decoder(x)
+        return x
+
+
+class ExpressCode(nn.Module):
+    def __init__(self):
+        super(ExpressCode, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv1d(1,5,4,stride=4),
+            nn.Tanh(),
+            nn.Conv1d(5,10,4,stride=4,padding=1),
+            nn.Tanh(),
+            nn.Conv1d(10,5,3,stride=3),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        batchsize = x.size()[0]
+        x = x.resize(batchsize,5)
+        return x
